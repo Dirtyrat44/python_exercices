d70a1e49a421f0f1fbd6850239b39b0cbde65439
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-06 21:30:17 +0000

    Sat, Jul 6, 2024, 11:30 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,13 +1,16 @@
 def main():
-    message = intput("Greeting: ")
-    print(f"${money(message)})
+    message = input("Greeting: ")
+    print(f"${money(message)}")
 
 
 def money(text):
     word = text.replace(",", " ").split(" ", 1).lower()
     if word == "hello":
         return "0"
-    elif word
+    elif word.startswith("h"):
+        return "20"
+    else:
+        return "100"
 
 
 
