f51fdece476c4b6ecb70439d9be09745591f3178
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-06 21:33:46 +0000

    Sat, Jul 6, 2024, 11:33 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,7 @@ def main():
 
 
 def money(text):
-    word = text.replace(",", " ").split(" ", 1).lower()
+    word = text.lower().replace(",", " ").split(" ", 1)
     if word == "hello":
         return "0"
     elif word.startswith("h"):
