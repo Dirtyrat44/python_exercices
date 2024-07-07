be2fccb91e11a426a9f37d0e45f6096cd26190b6
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-07 20:50:28 +0000

    Sun, Jul 7, 2024, 10:50 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,10 +1,17 @@
 def main():
-    print(f"Amount Due: ")
+    amount = 50
+    amount_due(amount)
+    user_input = input("Insert Coin: ")
+    while amount_due != 0:
+        amount_due(amount)
+        while user_input != 25 or user_input != 10 or user_input != 5:
+            
 
 
 
 
-
+def amount_due(n):
+    print(f"Amount Due: {n}")
 
 
 
