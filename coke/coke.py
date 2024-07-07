e5ed70839d6a7ea1a0124eba1d0c305250664529
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-07 20:54:13 +0000

    Sun, Jul 7, 2024, 10:54 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,8 +4,18 @@ def main():
     user_input = input("Insert Coin: ")
     while amount_due != 0:
         amount_due(amount)
-        while user_input != 25 or user_input != 10 or user_input != 5:
-            
+        user_input = input("Insert Coin: ")
+        while True:
+            if user_input == 25:
+                return amount - 25
+            elif user_input == 10:
+                return amount - 10
+            elif user_input == 5:
+                return amount - 5
+            else:
+                return True
+
+
 
 
 
