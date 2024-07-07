7fd648e514531e8a164cda7c34be318eeed41ac8
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-07 21:23:40 +0000

    Sun, Jul 7, 2024, 11:23 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,12 +4,8 @@ def main():
     while amount > 0:
         amount_due(amount)
         user_input = int(input("Insert Coin: "))
-        if user_input == 25:
-            amount -= 25
-        elif user_input == 10:
-            amount -= 10
-        elif user_input == 5:
-            amount -= 5
+        if user_input in [25, 10, 5]:
+            amount -= user_input
 
 
     print(f"Change Owed: {amount * -1}")
