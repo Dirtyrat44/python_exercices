e32b0a28fa0e13855eb16a773627759935bd7ad5
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-08 20:03:26 +0000

    Mon, Jul 8, 2024, 10:03 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,9 @@
 def main():
     user_input = input("Input: ")
-    print(vowels_eraser(user_input))
+    print(vowel_eraser(user_input))
 
 # Function to erase any vowels
-def vowels_eraser(str):
+def vowel_eraser(str):
     new_str = []
     for c in str:
         if c.lower() in ["a", "e", "i", "o", "u"]:
