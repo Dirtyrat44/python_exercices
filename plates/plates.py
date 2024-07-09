616f11f1f514dd793f67f8fef3890053f157b535
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-09 18:38:28 +0000

    Tue, Jul 9, 2024, 8:38 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,7 +9,10 @@ def main():
 def is_valid(s):
     if range(s) >= 2 and range(s) <= 6:
         if s[0, 2].isalpha():
-            
+            if s[-1].isdigit():
+                 return True
+            else:
+                 return False
 
         else:
             return False
