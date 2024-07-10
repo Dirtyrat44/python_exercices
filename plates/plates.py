822ecd356d275adb74753dc49332e45989de6cad
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-10 19:49:23 +0000

    Wed, Jul 10, 2024, 9:49 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -14,6 +14,8 @@ def is_valid(s):
         for c in s:
             if c.isdigit():
                 new_s.append(c)
+        if new_s[0:1] == "0" and is not new_s.isdigit():
+            return False
 
 
 
