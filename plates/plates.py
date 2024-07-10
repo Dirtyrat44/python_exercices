76b915f24aa87afc26d348900dee4b60350f6ead
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-10 20:29:17 +0000

    Wed, Jul 10, 2024, 10:29 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,25 +7,21 @@ def main():
 
 
 def is_valid(s):
-
-    # Check if first 2 are alpha, if min len is 2 and max len is 6 and if s doesn't contain ponctuation
-    if s[0:2].isalpha() and 6 >= len(s) > 1 and all (c not in [" ", "'", "."] for c in s):
+    # Vérifiez si les deux premiers sont alphabétiques, si la longueur est entre 2 et 6 et s'il n'y a pas de ponctuation
+    if s[0:2].isalpha() and 2 <= len(s) <= 6 and all(c not in [" ", "'", "."] for c in s):
         digit_seen = False
         for i, c in enumerate(s):
             if c.isdigit():
                 digit_seen = True
-                if c == "0" or not s[i:].isdigit():
+                # Si le premier chiffre est '0' ou s'il y a des non-chiffres après un chiffre
+                if c == '0' or not s[i:].isdigit():
                     return False
-            break
-        if not digit_seen:
+                break
+        if not digit_seen:  # Si aucun chiffre n'est trouvé, cela reste valide tant que c'est alphabétique
             return True
-        return digit_seen
-
-
-
+        return digit_seen  # Retourne True si un chiffre a été vu et toutes les vérifications sont passées
     else:
         return False
-
 
 
 
