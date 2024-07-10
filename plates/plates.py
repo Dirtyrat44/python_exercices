1a2faaf0a1232ef6a63012af22446e01d6a51978
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-10 19:44:22 +0000

    Wed, Jul 10, 2024, 9:44 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,7 +9,8 @@ def main():
 def is_valid(s):
     new_s = []
     new_s_digit = []
-    if s[0:2].isalpha() and 6 >= len(s) > 1 and s not in [".", " ", "'"]:
+    # Check if first 2 are alpha, if min len is 2 and max len is 6 and if s doesn't contain ponctuation
+    if s[0:2].isalpha() and 6 >= len(s) > 1 and all (c not in [" ", "'", "."] for c in s):
         for c in s:
             new_s.append(c)
 
