83a8f099644a5b404cc2d0d94c2bc4f522e12de7
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-11 15:02:17 +0000

    Thu, Jul 11, 2024, 5:02 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,9 +8,19 @@ def main():
 
 def is_valid(s):
     if s[0:2].isalpha() and 1 < len(s) < 7 and all(c not in [" ", ",", ".", "'"] for c in s):
-        for i, c in enumerate(s):
+        is_digit = False
+        for i, c in enumerate(s, start=2):
             if c.isdigit():
-                
+                if s[i:].isdigit():
+                    is_digit = True
+                    break
+                else:
+                    return False
+        if is_digit == False:
+            return True
+
+
+
 
 
 
