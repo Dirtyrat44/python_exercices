0f242f88d416945d8bd10f57a63b09d8f3617390
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-11 15:18:58 +0000

    Thu, Jul 11, 2024, 5:18 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,3 +1,5 @@
+
+
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -5,32 +7,21 @@ def main():
     else:
         print("Invalid")
 
-
 def is_valid(s):
-    if s[0:2].isalpha() and 1 < len(s) < 7 and all(c not in [" ", ",", ".", "'"] for c in s):
-        is_digit = False
-        for i, c in enumerate(s, start=2):
+    # Vérifiez si les deux premiers sont alphabétiques, si la longueur est entre 2 et 6 et s'il n'y a pas de ponctuation
+    if s[0:2].isalpha() and 2 <= len(s) <= 6 and all(c not in [" ", "'", "."] for c in s):
+        digit_seen = False
+        for i, c in enumerate(s):
             if c.isdigit():
-                is_digit = True
-                if c == "0" or not s[i:].isdigit():
-                   return False
+                digit_seen = True
+                # Si le premier chiffre est '0' ou s'il y a des non-chiffres après un chiffre
+                if c == '0' or not s[i:].isdigit():
+                    return False
                 break
-
-        if not is_digit:
+        if not digit_seen:  # Si aucun chiffre n'est trouvé, cela reste valide tant que c'est alphabétique
             return True
-
-        return is_digit
+        return digit_seen  # Retourne True si un chiffre a été vu et toutes les vérifications sont passées
     else:
         return False
 
-
-
-
-
-
-
-
-
 main()
-
-
