0f3e631ce5e89fecd981ebc38aed082de93a2a0e
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-12 12:57:28 +0000

    Fri, Jul 12, 2024, 2:57 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,7 @@
 def main():
     while True:
         user_input = input("Fraction: ")
-        if user_input.isalpha():
+        if user_input.isalpha() and len(user_input) < 2:
             pass
         else:
             break
@@ -15,4 +15,58 @@ def main():
 
 
 
+
+
+
+
 main()
+
+
+
+def main():
+    extract_fractions()
+
+    print()
+
+
+
+
+
+
+
+def extract_fractions():
+    while True:
+        try:
+            user_input = input("Fraction: ")
+            n1, n2 = user_input.split("/")
+        except:
+            return
+        else:
+            n1 = convert_int(n1)
+            n2 = convert_int(n2)
+
+
+
+def divide_return_percentage(n1, n2):
+    while True:
+            try:
+                n1 / n2
+            except ZeroDivisionError:
+                print("You cannot divide by zero")
+
+            else:
+                return round((n1 / n2) * 100)
+
+def convert_int(n):
+    while True:
+        try:
+            n = int(n)
+        except ValueError:
+            print(f"'{n}' is not an intenger")
+            break
+        else:
+            return n
+
+
+
+
