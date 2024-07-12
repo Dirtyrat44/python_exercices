2ce544d662308951d69f2ea5c83ec0b7244664ea
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-12 09:59:38 +0000

    Fri, Jul 12, 2024, 11:59 AM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,16 +1,19 @@
 def main():
     user_input = input = ("Fraction: ")
 
-    print(fuel_gauge(user_input))
+    print(fuel_gauge(user_input, "%", sep=""))
 
 
 
 def fuel_gauge(t):
-    t.split("/", 1)
+    n1, n2 = t.split("/", 1)
+    convert_int(n1)
+    convert_int(n2)
+    return ((n1 / n2) * 100)
 
-
-
-
+def convert_int(n):
+    try:
+        float(n)
 
 
 
