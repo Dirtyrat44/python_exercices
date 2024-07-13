469c6cc305c79aff073e2b4c84699966bb06eb9b
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-13 22:00:35 +0000

    Sun, Jul 14, 2024, 12:00 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,8 +18,11 @@ def main():
         user_input = input("Date: ")
         if user_input.replace("/", "").isdigit() and "/" in user_input:
             month, day, year = map(int, user_input.replace("/", " ").split(" "))
-            print(f"{year}-{month:02}-{day:02}")
-            break
+            if month < 13 and day < 32:
+                print(f"{year}-{month:02}-{day:02}")
+                break
+            else:
+                pass
         elif "," in user_input:
             try:
                 month, day, year = user_input.replace(",", "").split(" ")
@@ -28,10 +31,13 @@ def main():
             except ValueError:
                 pass
             else:
-                if month in months:
-                    #print month index +1
-                    print(f"{year}-{months.index(month) + 1:02}-{day:02}")
-                    break
+                if month < 13 and day < 32:
+                    if month in months:
+                        #print month index +1
+                        print(f"{year}-{months.index(month) + 1:02}-{day:02}")
+                        break
+                    else:
+                        pass
                 else:
                     pass
 
