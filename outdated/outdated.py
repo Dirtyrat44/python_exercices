037b2646910b1d1bc84648dcb125403e0f5b34c5
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-13 19:21:11 +0000

    Sat, Jul 13, 2024, 9:21 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,4 +1,21 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
 def main():
+    while True:
+        user_input = input("Date: ")
 
 
 
