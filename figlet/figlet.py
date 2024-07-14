b1f9cca324b6cd847e479e3fbe318c9107903115
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-14 19:37:26 +0000

    Sun, Jul 14, 2024, 9:37 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,8 +8,11 @@ figlet_available = figlet.getFonts()
 
 
 def main():
-    if len(sys.argv) == 0:
-        sys.exit()
+    if len(sys.argv) == 1:
+        s = input("Input: ")
+        font_choice = choice(figlet_available)
+        figlet.setFont(font=font_choice)
+        print(f"Output: {figlet.renderText(s)}")
 
 
     elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in figlet_available:
