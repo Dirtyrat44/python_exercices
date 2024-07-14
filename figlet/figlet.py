2a0b2624801c7e2dd704baa7f725a19c1c953636
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-14 19:38:37 +0000

    Sun, Jul 14, 2024, 9:38 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,7 +6,6 @@ figlet = Figlet()
 figlet_available = figlet.getFonts()
 
 
-
 def main():
     if len(sys.argv) == 1:
         s = input("Input: ")
@@ -14,20 +13,17 @@ def main():
         figlet.setFont(font=font_choice)
         print(f"Output: {figlet.renderText(s)}")
 
-
-    elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in figlet_available:
-            s = input("Input: ")
-            figlet.setFont(font=sys.argv[2])
-            print(f"Output: {figlet.renderText(s)}")
-
-
-
-
-
+    elif (
+        len(sys.argv) == 3
+        and sys.argv[1] in ["-f", "--font"]
+        and sys.argv[2] in figlet_available
+    ):
+        s = input("Input: ")
+        figlet.setFont(font=sys.argv[2])
+        print(f"Output: {figlet.renderText(s)}")
 
     else:
         sys.exit("Invalid usage")
-
 
 
 if __name__ == "__main__":
