47ee0c64386831acd0e1b6297bda69d370ef25e4
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-19 19:02:37 +0000

    Fri, Jul 19, 2024, 9:02 PM +02:00

--- game/game.py
+++ game/game.py
@@ -10,16 +10,27 @@ def main():
 
         if level > 0:
             guess_number = random.randint(1, level)
-            try:
-                guess = int(input("Guess: "))
-            except ValueError:
-                pass
-            if guess > 0:
-                
+            while True:
+                try:
+                    guess = int(input("Guess: "))
+                except ValueError:
+                    pass
+                if guess > 0:
+                    print(is_guess_true(level, guess))
+
+                else:
+                    pass
 
 
 
 
+def is_guess_true(n_to_find, n):
+    if n > n_to_find:
+        return "Too large!"
+    elif n < n_to_find:
+        return "Too small!"
+    else:
+        return "Just right!"
 
 if __name__ == "__main__":
     main()
