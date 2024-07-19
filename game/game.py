894e841572d1ba2c634dc74ba451f0f87ccaebb1
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-19 18:46:59 +0000

    Fri, Jul 19, 2024, 8:46 PM +02:00

---
+++ game/game.py
@@ -0,0 +1,14 @@
+def main():
+
+    while True:
+        try:
+            level = int(input("Level: "))
+        except ValueError:
+            break
+
+
+
+
+
+if __name__ == "__main__":
+    main()
