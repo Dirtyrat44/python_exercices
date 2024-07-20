e301e9848bf8e9b21a5a49df007bf947d9253364
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-20 11:17:29 +0000

    Sat, Jul 20, 2024, 1:17 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -18,8 +18,8 @@ def get_argument():
 def main():
     argument = get_argument()
     argument_json = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    print(argument_json.raise_for_status())
-    print(argument_json)
+
+    print(json.dump(argument_json.json()))
 
 
 
