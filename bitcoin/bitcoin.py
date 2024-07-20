0bb782a6d154ba109e2e55b5b3363fdd77875256
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-20 11:45:20 +0000

    Sat, Jul 20, 2024, 1:45 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -15,13 +15,14 @@ def get_argument():
         return sys.argv[1]
 
 def get_bitcoin_rate():
-    argument = get_argument()
+
     argument_json = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     o = argument_json.json()
-    return rate_float = o["bpi"]["USD"]["rate_float"]
+    return o["bpi"]["USD"]["rate_float"]
 
 def main():
-    ...
+    rate_float = get_bitcoin_rate()
+    argument = get_argument()
 
 
 
