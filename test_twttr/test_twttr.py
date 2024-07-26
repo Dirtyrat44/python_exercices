b1fc4d5727aa2157cd1b8bde32cd4199109eb642
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 07:43:50 +0000

    Fri, Jul 26, 2024, 9:43 AM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,15 +1,21 @@
 from twttr import shorten
 
-
-def test_shorten_str():
+def test_shorten_vowels():
     assert shorten("twitter") == "twttr"
     assert shorten("test") == "tst"
+    assert shorten("Twitter") == "Twttr"
+    assert shorten("AEIOU") == ""
+    assert shorten("aeiou") == ""
 
-def test_shorten_int():
+def test_shorten_non_vowels():
+    assert shorten("bcdfg") == "bcdfg"
+    assert shorten("BCDFG") == "BCDFG"
+
+def test_shorten_punctuation():
+    assert shorten("twttr!") == "twttr!"
+    assert shorten("hello, world!") == "hll, wrld!"
+
+def test_shorten_numbers():
+    assert shorten("12345") == "12345"
     assert shorten("5") == "5"
     assert shorten("0") == "0"
-
-def test_shorten_capitalize():
-    assert shorten("Twitter") == "Twttr"
-
-
