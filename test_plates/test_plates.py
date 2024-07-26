391e92cd14c7690b60a365771541ef5080db6b31
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 08:15:05 +0000

    Fri, Jul 26, 2024, 10:15 AM +02:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,20 @@
 from plates import is_valid
 
+def test_plates():
+    assert is_valid("CS50") == True
+
+def test_plates_non_alphabetical():
+    assert is_valid("00") == False
+
+def test_plates_lenght():
+    assert is_valid("CS50") == True
+    assert is_valid("C") == False
+    assert is_valid("CSSSS50") == False
+
+def test_plates_number_placement():
+    assert is_valid("CS50S") == False
+
+def test_plates():
+    assert is_valid("CS05") == False
+
 
