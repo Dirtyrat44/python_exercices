1f9c42fe74efbdc9a4c14c63f6c0cec445d93594
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 09:08:37 +0000

    Fri, Jul 26, 2024, 11:08 AM +02:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -6,14 +6,14 @@ def main():
 
 
 def convert(s):
-    numerator, denominator = map(int, s.split("/"))
-    if numerator > denominator:
-        return None
-    else:
+    while True:
         try:
-            return round((numerator / denominator) * 100)
-        except ZeroDivisionError:
-            return None
+            numerator, denominator = map(int, s.split("/"))
+
+
+
+        return round((numerator / denominator) * 100)
+
 
 
 
