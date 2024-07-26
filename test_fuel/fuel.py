0b3e4dbf0db8a32b7007188492f98bbf51f66efe
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 08:54:19 +0000

    Fri, Jul 26, 2024, 10:54 AM +02:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,34 @@
+def main():
+    while True:
+        user_input = input("Fraction: ")
+        if user_input.replace("/", "").isdigit() and "/" in user_input:
+            try:
+                percentage = convert(user_input)
+                if percentage is not None:
+                    print(format_response(percentage))
+                    break
+            except ValueError:
+                break
+
+
+def convert(s):
+    numerator, denominator = map(int, s.split("/"))
+    if numerator > denominator:
+        return None
+    else:
+        try:
+            return round((numerator / denominator) * 100)
+        except ZeroDivisionError:
+            return None
+
+
+
+def format_response(n):
+    if n in [99, 100]:
+        return "F"
+    elif n in [0, 1]:
+        return "E"
+    else:
+        return f"{n}%"
+
+main()
