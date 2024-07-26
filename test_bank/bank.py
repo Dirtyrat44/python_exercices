abcb3314a8d9a26bc1e8089523055868038f7387
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 08:04:42 +0000

    Fri, Jul 26, 2024, 10:04 AM +02:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -7,11 +7,11 @@ def value(text):
     text = text.strip().lower().replace(",", " ")
     word = text.split(" ", 1)[0]
     if word == "hello":
-        return "0"
+        return 0
     elif word.startswith("h"):
-        return "20"
+        return 20
     else:
-        return "100"
+        return 100
 
 
 if __name__ == "__main__":
