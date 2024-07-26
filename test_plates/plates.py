889459f42a68a07f0b45c58fb50cc7e3821d0292
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 08:08:33 +0000

    Fri, Jul 26, 2024, 10:08 AM +02:00

---
+++ test_plates/plates.py
@@ -0,0 +1,35 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if s[0:2].isalpha() and 1 < len(s) < 7 and all(c not in [" ", ",", ".", "'"] for c in s):
+        is_digit = False
+        for i, c in enumerate(s):
+
+            if c.isdigit():
+                is_digit = True
+                if c == "0" or not s[i:].isdigit():
+                   return False
+                break
+
+        if not is_digit:
+            return True
+
+        return is_digit
+    else:
+        return False
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
