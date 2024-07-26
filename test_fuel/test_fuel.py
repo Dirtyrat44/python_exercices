f046f49bd68f54612e9cb38ae1fd868889e51ff8
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 10:01:51 +0000

    Fri, Jul 26, 2024, 12:01 PM +02:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,5 +1,11 @@
 from fuel import convert, gauge
-
+import pytest
 
 def test_correct_input():
     assert convert("1/4") == 25 and gauge(25) == "25%"
+    assert convert("1/100") == 1 and gauge(1) == "E"
+    assert convert("99/100") == 99 and gauge(99) == "F"
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
