3afbcbe19b89e95c687c9bcd25ba66b2b9b8aefc
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 07:27:57 +0000

    Fri, Jul 26, 2024, 9:27 AM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,5 +1,10 @@
 from twttr import shorten
 
 
-def test_shorten():
-    assert shorten()
+def test_shorten_str():
+    assert shorten("twitter") == "twttr"
+    assert shorten("test") == "tst"
+
+def test_shorten_int():
+    assert shorten("5") == "5"
+    assert shorten("0") == "0"
