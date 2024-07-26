47b185765081940c494b3e4b486c216baf8a1c88
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-26 08:01:06 +0000

    Fri, Jul 26, 2024, 10:01 AM +02:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,1 +1,14 @@
-import 
+from bank import value
+
+def test_value_0():
+    assert value("Hello") == "0"
+    assert value("hello") == "0"
+    assert value("hello, my name is arthur") == "0"
+
+def test_value_20():
+    assert value("How are you ?") == "20"
+    assert value("Hi") == "20"
+
+def test_value_100():
+    assert value("Arthur") == "100"
+    assert value("What ?") == "100"
