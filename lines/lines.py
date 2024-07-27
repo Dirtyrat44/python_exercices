bb36e9e838f76c381cd7a40880751b3e3aac719d
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-27 14:10:29 +0000

    Sat, Jul 27, 2024, 4:10 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -10,7 +10,9 @@ def is_argv_ok():
         sys.exit("Too few command-lines argument")
     elif len(sys.argv) > 2:
         sys.exit("Too many command-line argument")
-        
+    elif len(sys.argv) == 2 and not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file")
+
     if len(sys.argv) == 2 and sys.argv[1].endswith(".py"):
         ...
 
