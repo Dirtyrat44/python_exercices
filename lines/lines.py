a877ab56bd239909fb9e47e8528cfbbffbad617e
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-27 14:08:50 +0000

    Sat, Jul 27, 2024, 4:08 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,13 +1,17 @@
 import sys
 
 def main():
-    ...
+    is_argv_ok()
 
 
 
 def is_argv_ok():
-    if len(sys.argv) <
-    if len(sys.argv) == 1 and sys.argv[1].endswith(".py"):
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-lines argument")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line argument")
+        
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".py"):
         ...
 
 if __name__ == "__main__":
