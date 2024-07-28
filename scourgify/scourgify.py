32107c283b8a0de4e4ca038c0e54861ef7ff10b2
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-28 13:43:01 +0000

    Sun, Jul 28, 2024, 3:43 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -7,5 +7,17 @@ def main():
 
 
 
+
+def check_arguments():
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    elif len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) == 3 and not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    if len(sys.argv) == 3 and sys.argv[1].endswith(".csv"):
+        return sys.argv[1]
+
 if __name__ == "__main__":
     main()
