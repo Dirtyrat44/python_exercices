5e64cc57c777574d43590124d3caadc09bbace9e
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-28 10:22:52 +0000

    Sun, Jul 28, 2024, 12:22 PM +02:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,17 +2,17 @@ import sys
 from tabulate import tabulate
 import csv
 
+
 def main():
     argument = check_argument()
     try:
         with open(argument) as file:
             reader = csv.DictReader(file)
+            # use a list of dictionnaries to format by keys
             data = list(reader)
             print(tabulate(data, headers="keys", tablefmt="grid"))
     except FileNotFoundError:
         sys.exit("Not a CSV file")
-
-
 
 
 # check if argument is valid
@@ -26,6 +26,7 @@ def check_argument():
 
     if len(sys.argv) == 2 and sys.argv[1].endswith(".csv"):
         return sys.argv[1]
+
 
 if __name__ == "__main__":
     main()
