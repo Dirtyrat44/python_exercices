ea1409760a8427a86665ef8876f0c18eca9f5de9
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-28 14:26:11 +0000

    Sun, Jul 28, 2024, 4:26 PM +02:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -9,21 +9,11 @@ def main():
             reader = csv.DictReader(before)
             writer = csv.DictWriter(after, fieldnames=["first", "last", "house"])
             writer.writeheader()
-            for row in  reader:
+            for row in reader:
                 last, first = row["name"].split(", ")
-                writer.writerow(
-                    {
-                        "first": first,
-                        "last": last,
-                        "house": row["house"]
-                    }
-                )
+                writer.writerow({"first": first, "last": last, "house": row["house"]})
     except FileNotFoundError:
         sys.exit(f"Could not read {sys.argv[1]}")
-
-
-
-
 
 
 def check_arguments():
@@ -31,11 +21,20 @@ def check_arguments():
         sys.exit("Too many command-line arguments")
     elif len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
-    elif len(sys.argv) == 3 and not sys.argv[1].endswith(".csv") or not sys.argv[2].endswith(".csv"):
+    elif (
+        len(sys.argv) == 3
+        and not sys.argv[1].endswith(".csv")
+        or not sys.argv[2].endswith(".csv")
+    ):
         sys.exit("Not a CSV file")
 
-    if len(sys.argv) == 3 and sys.argv[1].endswith(".csv") and sys.argv[2].endswith(".csv"):
+    if (
+        len(sys.argv) == 3
+        and sys.argv[1].endswith(".csv")
+        and sys.argv[2].endswith(".csv")
+    ):
         return sys.argv[1], sys.argv[2]
+
 
 if __name__ == "__main__":
     main()
