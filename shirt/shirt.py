18c37d68bbfdfb2d7a2b430316eb0ee12b83b8ab
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-28 15:55:43 +0000

    Sun, Jul 28, 2024, 5:55 PM +02:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,12 +1,8 @@
 import sys
+import os
 
 
-def main():
-    ...
-
-
-
-
+def main(): ...
 
 
 def check_arguments():
@@ -14,10 +10,25 @@ def check_arguments():
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) < 3:
         sys.exit("Too many command-line arguments")
-    elif len(sys.argv) == 3 and os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
+    elif len(sys.argv) == 3 and os.path.splitext(sys.argv[1]) != os.path.splitext(
+        sys.argv[2]
+    ):
         sys.exit("Input and output have different extensions")
-    elif len(sys.argv) == 3 and not sys.argv[1].endswith(".jpg", ".jpeg", ".png").lower() and not sys.argv[2].endswith(".jpg", ".jpeg", ".png").lower():
+    elif (
+        len(sys.argv) == 3
+        and not sys.argv[1].endswith(".jpg", ".jpeg", ".png").lower()
+        and not sys.argv[2].endswith(".jpg", ".jpeg", ".png").lower()
+    ):
         sys.exit("Invalid input")
+
+    if (
+        len(sys.argv) == 3
+        and sys.argv[1].endswith(".jpg", ".jpeg", ".png").lower()
+        and sys.argv[2].endswith(".jpg", ".jpeg", ".png").lower()
+        and os.path.splitext(sys.argv[1]) == os.path.splitext(sys.argv[2])
+    ):
+        return sys.argv[1], sys.argv[2]
+
 
 if __name__ == "__main__":
     main()
