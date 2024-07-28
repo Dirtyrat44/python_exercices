fe7d5c11135c0fd6efff22a69f9f7d26546196a6
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-07-28 15:53:00 +0000

    Sun, Jul 28, 2024, 5:53 PM +02:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -15,7 +15,9 @@ def check_arguments():
     elif len(sys.argv) < 3:
         sys.exit("Too many command-line arguments")
     elif len(sys.argv) == 3 and os.path.splitext(sys.argv[1]) != os.path.splitext(sys.argv[2]):
-        sys.exit("")
+        sys.exit("Input and output have different extensions")
+    elif len(sys.argv) == 3 and not sys.argv[1].endswith(".jpg", ".jpeg", ".png").lower() and not sys.argv[2].endswith(".jpg", ".jpeg", ".png").lower():
+        sys.exit("Invalid input")
 
 if __name__ == "__main__":
     main()
