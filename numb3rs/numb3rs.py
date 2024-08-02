120c4d25e8c3be09950764fdf6c40a48c3937c8c
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 21:37:52 +0000

    Fri, Aug 2, 2024, 11:37 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,31 +1,22 @@
 import re
-import sys
-
 
 def main():
     print(validate(input("IPv4 Address: ")))
 
-
 def validate(ip):
     matches = re.search(r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip)
     if matches:
-        if check_number(matches.group(1)) and check_number(matches.group(2)) and check_number(matches.group(3)) and check_number(matches.group(4)):
-            return "True"
-        else:
-            return"False"
-
-    else:
-        return "False"
+        return all(check_number(matches.group(i)) for i in range(1, 5))
+    return False
 
 def check_number(n):
     try:
         n = int(n)
+        if 0 <= n <= 255:
+            return True
+        return False
     except ValueError:
         return False
-    else:
-        return 0 <= n <= 255
-
-
 
 if __name__ == "__main__":
     main()
--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,9 +2,9 @@ import pytest
 from numb3rs import validate, check_number
 
 def test_validate():
-    assert validate("1.1.1.1") == "True"
-    assert validate("256.256.256.256") == "False"
-    assert validate("abc.def.ghi.jkl") == "False"
+    assert validate("1.1.1.1") == True
+    assert validate("256.256.256.256") == False
+    assert validate("abc.def.ghi.jkl") == False
 
 def test_check_number():
     assert check_number("1") == True
