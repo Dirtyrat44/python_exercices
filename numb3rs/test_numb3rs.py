7b0d4b5d2c5cb11efd79d93ce149ad648f002b5e
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 21:55:29 +0000

    Fri, Aug 2, 2024, 11:55 PM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,6 +5,7 @@ from numb3rs import validate
 
 def test_ip_format():
     assert validate(r"0.0.0.0") == True
+    assert validate(r"0") == False
 
 def test_ip_range():
     assert validate(r"255.0.0.100") == True
