118bf163baa56256c23502a33eefd8dfb8be1ecb
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 19:25:56 +0000

    Fri, Aug 2, 2024, 9:25 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -3,19 +3,19 @@ import sys
 
 
 def main():
-    validate(input("IPv4 Address: "))
+    print(validate(input("IPv4 Address: ")))
 
 
 def validate(ip):
     matches = re.search(r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip)
     if matches:
         if check_number(matches.group(1)) and check_number(matches.group(2)) and check_number(matches.group(3)) and check_number(matches.group(4)):
-            sys.exit("True")
+            return "True"
         else:
-            sys.exit("False")
+            return"False"
 
     else:
-        sys.exit("False")
+        return "False"
 
 def check_number(n):
     n = int(n)
