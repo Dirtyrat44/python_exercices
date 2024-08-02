7cd0c41539f3e46365b5fe9e42499d67a8db6bd3
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 19:13:57 +0000

    Fri, Aug 2, 2024, 9:13 PM +02:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -9,13 +9,16 @@ def main():
 def validate(ip):
     matches = re.search(r"^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip)
     if matches:
-        print(matches.group(1))
-        sys.exit("True")
+        if check_number(matches.group(1)) and check_number(matches.group(2)) and check_number(matches.group(3)) and check_number(matches.group(4)):
+            sys.exit("True")
+        else:
+            sys.exit("False")
 
     else:
         sys.exit("False")
 
-
+def check_number(n):
+    return 0 <= n <= 255
 
 
 
