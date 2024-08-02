05f150b42d69cf52ba19c3e325c077a05fa5d4a4
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 21:26:39 +0000

    Fri, Aug 2, 2024, 11:26 PM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,7 +2,7 @@ import pytest
 from numb3rs import validate, check_number
 
 def test_validate():
-    assert validate("1.1.1.1") == True
+    assert validate("1.1.1.1") == "True"
 
 def test_check_number():
     assert check_number("1") == True
