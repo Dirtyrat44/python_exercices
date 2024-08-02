3c65db66f3885b742ecbd3be484005ef1afd565e
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 21:36:22 +0000

    Fri, Aug 2, 2024, 11:36 PM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -3,10 +3,14 @@ from numb3rs import validate, check_number
 
 def test_validate():
     assert validate("1.1.1.1") == "True"
+    assert validate("256.256.256.256") == "False"
+    assert validate("abc.def.ghi.jkl") == "False"
 
 def test_check_number():
     assert check_number("1") == True
+    assert check_number("255") == True
+    assert check_number("256") == False
+    assert check_number("-1") == False
 
 def test_check_number_alpha():
-    with pytest.raises(ValueError):
-        check_number("cat")
+    assert check_number("cat") == False
