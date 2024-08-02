d047f1677196c3fd946517ea9e75e8267c758a2e
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-02 21:51:50 +0000

    Fri, Aug 2, 2024, 11:51 PM +02:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -17,6 +17,7 @@ def test_validate_range():
     assert validate("1.1.256.1") == False
     assert validate("1.1.1.256") == False
     assert validate("255.255.255.255") == True
+    assert validate("512.1.1.1") == False
 def test_check_number():
     assert check_number("1") == True
     assert check_number("255") == True
