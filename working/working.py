bb1937f7055842b6d7007a8be1274662e40309f9
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-03 09:22:13 +0000

    Sat, Aug 3, 2024, 11:22 AM +02:00

--- working/working.py
+++ working/working.py
@@ -7,14 +7,13 @@ def main():
 
 
 def convert(s):
-    matches = re.search("^((\d+:\d+|\d+)\s+(AM|PM))", s, re.IGNORECASE)
+    matches = re.search(r"^((\d+:\d+|\d+)\s+(AM|PM))", s, re.IGNORECASE)
     return matches.group(1)
 
 
 
 def find_am_pm(s):
-    matches = re.search("^((\d+|\d+:\d+)\s+(AM|PM))", s, re.IGNORECASE)
-    return matches.group(1)
+    ...
 
 
 if __name__ == "__main__":
