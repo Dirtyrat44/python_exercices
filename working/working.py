80613a39dac47b6921cde2258fb763767bedb2ce
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-03 09:04:08 +0000

    Sat, Aug 3, 2024, 11:04 AM +02:00

--- working/working.py
+++ working/working.py
@@ -11,7 +11,7 @@ def convert(s):
 
 
 def find_am_pm(s):
-    matches = re.search("", s, re.IGNORECASE)
+    matches = re.search("^([0-9]|(1[10-2])", s, re.IGNORECASE)
 
 
 if __name__ == "__main__":
