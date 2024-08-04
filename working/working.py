13924967c2b4091935584be223bf1dbd5075c9f6
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-04 18:46:41 +0000

    Sun, Aug 4, 2024, 8:46 PM +02:00

--- working/working.py
+++ working/working.py
@@ -9,16 +9,13 @@ def main():
 def convert(s):
     matches = re.search(r"^((\d+:\d+|\d+)\s+(AM|PM))\s+to\s+((\d+:\d+|\d+)\s+(AM|PM))+", s, re.IGNORECASE)
     if matches:
-        hour_1, hour_2 = find_am_pm(matches.group(2), matches.group(3)), find_am_pm(matches.group(5), matches.group(6))
+        hour_1, am_pm_1, hour_2, am_pm_2 = matches.group(1, 2, 3, 4)
         return f"{hour_1} to {hour_2}"
 
 
 
 def find_am_pm(n, s):
-    if s.lower() == "am":
-        return f"{int(n):02} AM"
-    if s.lower() == "pm":
-        return f"{(int(n) + 12):02} PM"
+
 
 
 
