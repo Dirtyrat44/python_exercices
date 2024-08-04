dfb53d19581c9b4fed369001fdb2164fc3cf5991
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-04 20:19:23 +0000

    Sun, Aug 4, 2024, 10:19 PM +02:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    matches = re.search(r"/^([0-9][0-2]?):?([0-5][0-9])? (AM|PM) to ([0-9][0-2]?):?([0-5][0-9])? (AM|PM)$/gm", s)
+    matches = re.search(r"/^([0-9][0-2]?):?([0-5][0-9])? (AM|PM) to ([0-9][0-2]?):?([0-5][0-9])? (AM|PM)$", s)
     if matches:
         hour_1, minute_1, am_pm_1 = matches.group(1, 2, 3)
         hour_2, minute_2, am_pm_2 = matches.group(4, 5, 6)
