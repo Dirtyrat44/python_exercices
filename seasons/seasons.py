ad667781bc53624bb1e123cdbba01118463f32c0
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-10 21:38:11 +0000

    Sat, Aug 10, 2024, 11:38 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,7 +1,10 @@
 from datetime import date
 import inflect  # translate numbers to words
+import sys
 
 p = inflect.engine()
+# words = p.number_to_words(1234, andword="")
+# "one thousand, two hundred thirty-four"
 
 
 def main():
