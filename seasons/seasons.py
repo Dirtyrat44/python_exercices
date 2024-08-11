b3cd2dcba37f1c72dd1c3a02157b1a642b0499ff
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-11 17:48:25 +0000

    Sun, Aug 11, 2024, 7:48 PM +02:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,14 +4,14 @@ import inflect  # translate numbers to words
 import sys
 
 p = inflect.engine()
-# words = p.number_to_words(1234, andword="")
-# "one thousand, two hundred thirty-four"
 
 
 def main():
     user_birth = check_date_format(input("Date of birth: "))
     days_difference = date.today() - user_birth
-    minutes_difference = (lambda x: 1440 * x.days)(days_difference)
+    minutes_difference = (lambda x: 1440 * x.days)(
+        days_difference
+    )  # lambda function 1440 minutes per day * days
     words = p.number_to_words(minutes_difference, andword="")
     print(f"{words.capitalize()} minutes")
 
@@ -33,7 +33,7 @@ def check_date_format(date_str):
         sys.exit("Invalid date")
     if month in [4, 6, 9, 11] and not 0 < day <= 30:
         sys.exit("Invalid date")
-    # check january month's days
+    # check february month's days
     if month == 2:
         if year % 2 == 0:
             if year % 100 == 0:
--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -4,5 +4,8 @@ from datetime import date
 
 def test_date_format():
     assert check_date_format("1993-1-8") == date(1993, 1, 8)
+    assert check_date_format("2020-1-10") == date(2020, 1, 10)
     with pytest.raises(SystemExit):
         check_date_format("cat")
+    with pytest.raises(SystemExit):
+        check_date_format("1993-1")
