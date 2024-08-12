e8bd7bcbe6de9c5e49558ef47135f427b5726761
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-12 11:32:50 +0000

    Mon, Aug 12, 2024, 1:32 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,10 +6,10 @@ def main():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('helvetica', 'B', 16)
-    pdf.cell(60, 10, 'Powered by FPDF.', new_x="LMARGIN", new_y="NEXT", align='C')
+    pdf.cell(50, 20, 'Powered by FPDF.', align='C')
     pdf.output("test.pdf")
 
-    
+
 
 
 
