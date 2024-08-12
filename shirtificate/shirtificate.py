3305b1b7578c8aa8ad0cbda682ad62533b33cebb
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-12 12:59:06 +0000

    Mon, Aug 12, 2024, 2:59 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,10 +4,10 @@ from fpdf.enums import Align
 class PDF(FPDF):
     def header(self):
 
-        # Setting font: helvetica bold 20
-        self.set_font("helvetica", "B", 25)
+        # Setting font: helvetica bold 50
+        self.set_font("helvetica", "B", 50)
         # Printing title:
-        self.cell(0, 40, "CS50 Shirtificate", align="C")
+        self.cell(0, 70, "CS50 Shirtificate", align="C")
         # Performing a line break:
         self.ln(20)
     def set_auto_page_break(self, auto, margin=0):
@@ -19,8 +19,8 @@ def main():
     pdf = PDF(format=(210, 297))
     pdf.add_page()
     pdf.image("shirtificate.png", x=15, y=78, w=180, keep_aspect_ratio=True)
-    pdf.set_font('helvetica', 'B', 12)
-    pdf.cell(0, 20, 'Powered by FPDF.', align='C')
+    pdf.set_font('helvetica', 'B', 25)
+    pdf.cell(0, 200, 'Arthur Poncin', align='C')
     pdf.output("test.pdf")
 
 
