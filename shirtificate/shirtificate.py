eab0ff77d654facff5a8dc6207eae385d082efb4
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-12 11:37:23 +0000

    Mon, Aug 12, 2024, 1:37 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,11 +1,23 @@
 from fpdf import FPDF
 
 
+class PDF(FPDF):
+    def header(self):
+
+        # Setting font: helvetica bold 16
+        self.set_font("helvetica", "B", 16)
+        # Moving cursor to the right:
+        self.cell(80)
+        # Printing title:
+        self.cell(30, 10, "CS50 Shirtificate", border=1, align="C")
+        # Performing a line break:
+        self.ln(20)
+
 
 def main():
-    pdf = FPDF()
+    pdf = PDF()
     pdf.add_page()
-    pdf.set_font('helvetica', 'B', 16)
+    pdf.set_font('helvetica', 'B', 12)
     pdf.cell(50, 20, 'Powered by FPDF.', align='C')
     pdf.output("test.pdf")
 
