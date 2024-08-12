315f6713ebdfdd333ce394e120b4ab2c4fee6c99
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-12 13:06:34 +0000

    Mon, Aug 12, 2024, 3:06 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,5 +1,4 @@
 from fpdf import FPDF
-from fpdf.enums import Align
 
 class PDF(FPDF):
     def header(self):
@@ -10,6 +9,7 @@ class PDF(FPDF):
         self.cell(0, 70, "CS50 Shirtificate", align="C")
         # Performing a line break:
         self.ln(20)
+
     def set_auto_page_break(self, auto, margin=0):
         self.auto = auto
         self.b_margin = margin
@@ -20,14 +20,10 @@ def main():
     pdf = PDF(format=(210, 297))
     pdf.add_page()
     pdf.image("shirtificate.png", x=15, y=78, w=180, keep_aspect_ratio=True)
-    pdf.set_font('helvetica', 'B', 25)
+    pdf.set_font("helvetica", "B", 25)
     pdf.set_text_color(r=255, g=255, b=255)
-    pdf.cell(0, 200, user_input, align='C')
+    pdf.cell(0, 200, user_input, align="C")
     pdf.output("shirtificate.pdf")
-
-
-
-
 
 
 if __name__ == "__main__":
