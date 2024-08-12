a87244923c21b2101321fe99a5825aa0ec51b516
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-12 09:29:32 +0000

    Mon, Aug 12, 2024, 11:29 AM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -24,5 +24,10 @@ class Jar:
 
     @property
     def size(self):
-        ...
+        return self._size
+    @size.setter
+    def size(self, size):
+        if self.size < 1 and self.size > self.capacity:
+            raise ValueError("Your jar capacity can't contains this much cookies")
+        self._size = size
 
