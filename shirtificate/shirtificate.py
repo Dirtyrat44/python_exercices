be7a640b61312b76d0671bfcb9424b7d951e7bdd
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-12 11:27:50 +0000

    Mon, Aug 12, 2024, 1:27 PM +02:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,7 +3,9 @@ from fpdf import FPDF
 
 
 def main():
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('helvetica', 'B', 16)
 
     user_input = input("Name: ")
 
