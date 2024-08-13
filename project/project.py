ed04a443546cf8aa38310ce5528e7a9143351474
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-13 12:25:21 +0000

    Tue, Aug 13, 2024, 2:25 PM +02:00

--- project/project.py
+++ project/project.py
@@ -3,7 +3,7 @@ import re
 import json
 
 def main():
-    response = requests.get("https://openfarm.cc/api/v1/crops?filter=tomato")
+    response = requests.get("https://openfarm.cc/api/v1/crops?filter=carrot")
     o = response.json()
     print(json.dumps(response.json(), indent=4))
 
