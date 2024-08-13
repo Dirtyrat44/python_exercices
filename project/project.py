75eaf6f5b8aacc2a7ff4544b610957867f2927b2
Dirtyrat44 <149596646+Dirtyrat44@users.noreply.github.com>
2024-08-13 10:42:38 +0000

    Tue, Aug 13, 2024, 12:42 PM +02:00

--- project/project.py
+++ project/project.py
@@ -1,8 +1,10 @@
 import requests
 import re
+import json
 
 def main():
-    ...
+    response = requests.get("https://openfarm.cc/api/v1/crops?filter=tomato")
+    print(json.dumps(response.json(), indent=4))
 
 
 def input_check():
